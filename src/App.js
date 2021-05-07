434c76e22c53fd933edd5fed1a191d9b248808c3
shhhQuiettt <krzysztof.skro@gmail.com>
2021-05-07 13:32:45 +0200

    Change invalid fields handling and create service.js
    Cleaned code
    CreateUser and validation functions are in service.js now
    Invalid fields are marked through conditional classes now
    More comments added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import './components/RegisterForm'
-import { useState } from 'react'
 
 import RegisterForm from './components/RegisterForm';
 
 
 
 function App() {
-
-  //const [registration, setRegistration] = useState(true)
 
   return (
     <div className="form-container">
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,51 +1,53 @@
-import {useState} from 'react'
-import {validate, validatePass} from "../validation"
-import axios from 'axios'
-
+import {useState, useEffect} from 'react'
+import {createUser, validate, validatePass} from "../service"
 
 const RegisterForm = () => {
 
     const [user, setUser] = useState({
-        "name": "Adam",
-	    "surname":"Mir",
-	    "email": ".adam.zez@wp.pl",
-	    "street": "Bosa",
-	    "number": "23",
-	    "city":"Poznań",
-	    "post_code": "60-125",
-	    "pass": ""
+        "name": "",
+        "surname":"",
+        "email": "",
+        "street": "",
+        "number": "",
+        "city":"",
+        "post_code": "",
+        "pass": ""
     })
 
-    const [passDiff, setPassDiff] = useState(false)
-    const [passStr, setPassStr] = useState(true)
-    const [invalid, setInvalid] = useState(false)
+    const [passDiff, setPassDiff] = useState(false) //true if "pass" and "pass-confirmation" are not equal
+    const [passStr, setPassStr] = useState(true) //true if password is strong enough
+    const [invalid, setInvalid] = useState(false) //true if after submiting at least one field is invalid
 
-    const handleSubmit = async (event) => {
+    const [passConf, setPassConf] = useState("") //contains pass_confirmation value
+
+    const [isValid, setIsValid] = useState({
+        "name": true,
+        "surname":true,
+        "email": true,
+        "street": true,
+        "number": true,
+        "city": true,
+        "post_code": true,
+        "pass": true   
+ 
+    })
+    
+    const handleSubmit = (event) => {
         event.preventDefault()
-        if(!passDiff &&passStr){
+        setInvalid(false)
+
+        if(!passDiff && passStr){
             
-            let err = validate(user)
-            // console.log(user)
-
-            if  (err.length!==0) { //make invalid red etc.
-                console.log("dup")
+            const err = validate(user) //err is an array of invalids user keys
+            if  (err.length!==0) { //make invalid fields red and pop info under form
                 setInvalid(true)
-                err.forEach((id)=> {
-                    let invalid = document.getElementById(id)
-                    invalid.style.backgroundColor = "#ff9999"
-                    invalid.onclick = (event) => {
-                        event.target.style.backgroundColor = ""
-                        setInvalid(false)
-                    }
-                })
+                setIsValid( {...err.reduce((res, item, _) => { //change all invalid keys to false
+                    res[item] = false
+                    return res
+                },isValid) } ) 
+
             }else {
-
-            axios.post("https://localhost:3001/registration",
-                    {"user": user},
-                    {wihCredentials:true},
-            ).then(response=>{console.log("registration res: ", response)}
-            ).catch(error => {console.log("registration error: ", error)})
-
+                createUser(user)            
             }
         }
            
@@ -54,49 +56,77 @@ const RegisterForm = () => {
 
     return (  
         <form id="register-form" onSubmit={event=>{handleSubmit(event)}}>
-            <div className="input-holder">
-                    <input id="name" type="text" 
+            <div className="input-holder" >
+                    <input id="name" type="text" className={`${isValid.name?"":"invalid-input"}`}
                         placeholder="Imię" value={user.name} 
-                        onChange={event => setUser({...user, name: event.target.value})} required/>
-                    <input id="surname" type="text" 
+                        onChange={event => setUser({...user, name: event.target.value})}
+                        onMouseDown={(event) => setIsValid({...isValid, name: true})}
+                        required/>
+
+                    <input id="surname" type="text" className={`${isValid.surname?"":"invalid-input"}`}
                         placeholder="Nazwisko" value={user.surname} 
-                        onChange={event => setUser({...user, surname: event.target.value})} required/> 
+                        onChange={event => setUser({...user, surname: event.target.value})} 
+                        onMouseDown={(event) => setIsValid({...isValid, surname: true})}
+                        required/> 
             </div>
 
             <input id="email" type="text" 
+                className={`${isValid.email?"":"invalid-input"}`}
                 placeholder="E-mail" value={user.email} 
-                onChange={event => setUser({...user, email: event.target.value})} required/>
+                onChange={event => setUser({...user, email: event.target.value})}
+                onMouseDown={(event) => setIsValid({...isValid, email: true})}
+                required/>
             
             <div className="input-holder">
                 <input id="street" type="text" placeholder="Ulica" 
-                    value={user.street} onChange={event => setUser({...user, street: event.target.value})} required/>
+                    className={`${isValid.street?"":"invalid-input"}`}
+                    value={user.street} 
+                    onChange={event => setUser({...user, street: event.target.value})}
+                    onMouseDown={(event) => setIsValid({...isValid, street: true})}
+                    required/>
 
-                <input id="number" type="text" placeholder="Numer" 
-                    value={user.number} onChange={event => setUser({...user, number: event.target.value})} required/>
+                <input id="number" type="text" placeholder="Numer"
+                    className={`${isValid.number?"":"invalid-input"}`} 
+                    value={user.number} 
+                    onChange={event => setUser({...user, number: event.target.value})}
+                    onMouseDown={(event) => setIsValid({...isValid, number: true})}
+                    required/>
                 
                 <input id="city" type="text" placeholder="Miasto"
-                    value={user.city} onChange={event => setUser({...user, city: event.target.value})} required/>
+                    className={`${isValid.city?"":"invalid-input"}`}
+                    value={user.city} 
+                    onChange={event => setUser({...user, city: event.target.value})}
+                    onMouseDown={(event) => setIsValid({...isValid, city: true})}
+                    required/>
 
                 <input id="post_code" type="text" placeholder="Kod pocztowy"
-                    value={user.post_code} onChange={event => setUser({...user, post_code: event.target.value})} required/>
+                    className={`${isValid.post_code?"":"invalid-input"}`}
+                    value={user.post_code} 
+                    onChange={event => setUser({...user, post_code: event.target.value})} 
+                    onMouseDown={(event) => setIsValid({...isValid, post_code: true})}
+                    required/>
 
             </div>
             
             <input id="pass" type="password" placeholder="Hasło"
-                value={user.pass} onChange={event => {
+                className={`${isValid.pass?"":"invalid-input"}`}
+                value={user.pass} 
+                onChange={event => {
                         setUser({...user, pass: event.target.value})
                         validatePass(event.target.value) ? setPassStr(true):setPassStr(false)
+                        passConf===event.target.value  && setPassDiff(false)
                 }} required/>           
             {!passStr && <p className="invalid">Hasło jest za słabe!</p>}
             
             
             <input id="pass-confirmation" type="password" placeholder="Powtórz hasło" 
-                onChange={event => {
+                value = {passConf} onChange={event => {
+                    setPassConf(event.target.value)
                     event.target.value!==user.pass ? setPassDiff(true) : setPassDiff(false)}
                  } required/>
             
             {passDiff && <p className="invalid">Hasła się nie zgadzają!</p>}
-            {invalid && <p className="invalid">Jedno lub więcej pól jest niepoprawnych!</p>}
+            {invalid && <p className="invalid">Jedno lub więcej pól jest nieprawidłowych!</p>}
             
             <input type="submit" form="register-form"  id="register-submit"  className="submit" 
                 value="Zarejestruj się!"/>
